d772c9380f04af1014ff0038bb248bc72f87e841
Fayyozbek Berdiyorov <fayyozbek@Fayyozbeks-MacBook-Pro.local>
2022-04-02 15:46:57 +0500

    fix serializer module

--- app/feedbacks/serializers.py
+++ app/feedbacks/serializers.py
@@ -9,6 +9,19 @@ class FeedbackSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id', 'created_at', 'changed_at', 'guid')
 
+    def create(self, validated_data):
+        module_id = validated_data.get("module")
+        teacher_id = validated_data.get("teacher")
+        if module_id:
+            module = Module.objects.get(id=module_id)
+            feedback = Feedback.objects.create(text=validated_data.get("text"), module=module)
+
+        # if teacher_guid:
+        #     teacher = Teacher.objects.get(guid=teacher_guid)
+        #     feedback  = Feedback.objects.create(text=validated_data.get("text"), module=teacher)
+
+            return feedback
+
 
 class TeacherSerializer(serializers.ModelSerializer):
 
